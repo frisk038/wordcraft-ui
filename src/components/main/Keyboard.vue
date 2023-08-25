868b888fdaa7d0114204d1b1e34554f087a6630c
frisk038 <frisktrentehuit@gmail.com>
2023-08-25 15:46:55 +0200

    number animation

--- src/components/main/Keyboard.vue
+++ src/components/main/Keyboard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import { NButton, NIcon } from 'naive-ui'
+import { NButton, NIcon, NGrid, NGridItem } from 'naive-ui'
 import { CloseSharp, CheckmarkSharp } from '@vicons/ionicons5'
 
 defineEmits(['type', 'valid'])
@@ -14,15 +14,20 @@ function getSvgPath(item) {
 </script >
 
 <template >
-    <n-button v-for="(item, index) in letters" :key="index" @click="$emit('type', item)" circle>
-        <template #icon>
-            <n-icon>
-                <svg viewBox="0 0 111 111">
-                    <use :xlink:href="getSvgPath(item)" />
-                </svg>
-            </n-icon>
-        </template>
-    </n-button>
+    <n-grid x-gap="5" :cols="3">
+        <n-grid-item>
+            <n-button v-for="(item, index) in letters" :key="index" @click="$emit('type', item)" circle>
+                <template #icon>
+                    <n-icon>
+                        <svg viewBox="0 0 111 111">
+                            <use :xlink:href="getSvgPath(item)" />
+                        </svg>
+                    </n-icon>
+                </template>
+            </n-button>
+        </n-grid-item>
+    </n-grid>
+
     <n-button @click="$emit('valid')" circle>
         <template #icon>
             <n-icon>
--- src/components/main/input.vue
+++ src/components/main/input.vue
@@ -1,33 +1,56 @@
 <script setup>
-import { NInput, NSpace, NIcon, NButton, NCard } from 'naive-ui'
+import { NInput, NSpace, NIcon, NButton, NCard, NSpin, NStatistic, NNumberAnimation, NLayout } from 'naive-ui'
 import { CloseSharp, CheckmarkSharp } from '@vicons/ionicons5'
+import { ref } from 'vue'
 
 const emit = defineEmits(['clear', 'newWord'])
 const props = defineProps(['word'])
 
+const score = ref(10)
+const checking = ref(false)
+const isValidated = ref(false)
+
 function check() {
-    console.log("check api")
+    if (props.word === "" || props.word.length < 2) return;
+
+    checking.value = true
+    console.log("checking if " + props.word + " is valid")
+    setTimeout(() => {
+        checking.value = false
+        isValidated.value = true
+    }, 2000);
     emit("newWord")
 }
 </script >
 <template>
-    <n-space justify="center" align="center">
-        <n-input type="text" size="large" placeholder="votre bafouille" round v-model:value="props.word" readonly />
-        <n-button circle @click="$emit('clear')">
-            <template #icon>
-                <n-icon>
-                    <CloseSharp />
-                </n-icon>
-            </template>
-        </n-button>
-        <n-button @click="check" circle>
-            <template #icon>
-                <n-icon>
-                    <CheckmarkSharp />
-                </n-icon>
-            </template>
-        </n-button>
-    </n-space>
+    <n-spin :show="checking" size="small">
+
+        <n-space justify="center" align="center">
+            <n-statistic label="Valeur">
+                <n-number-animation :active="isValidated" :from="0" :to="score" />
+            </n-statistic>
+
+            <n-input type="text" size="large" placeholder="votre mot" round v-model:value="props.word" readonly />
+
+            <n-button :disabled="isValidated" circle @click="$emit('clear')">
+                <template #icon>
+                    <n-icon>
+                        <CloseSharp />
+                    </n-icon>
+                </template>
+            </n-button>
+
+            <n-button @click="check" :disabled="isValidated" circle>
+                <template #icon>
+                    <n-icon>
+                        <CheckmarkSharp />
+                    </n-icon>
+                </template>
+            </n-button>
+
+        </n-space>
+
+    </n-spin>
 </template>
 <script>
 
